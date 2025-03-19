8cfa90f9b05dbc297c6a49c216005563a0037ee0
Magnus Jensen <magnusjensen@MediaMac.local>
2025-03-19 11:39:51 +0100

    Lowered the tools version and kept strict concurrency in swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,12 +17,18 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "FlowNavigation"
+            name: "FlowNavigation",
+            swiftSettings: [
+                .unsafeFlags(["-strict-concurrency=complete"])
+            ]
         ),
-        
         .testTarget(
             name: "FlowNavigationTests",
-            dependencies: ["FlowNavigation"]
+            dependencies: ["FlowNavigation"],
+            swiftSettings: [
+                .unsafeFlags(["-strict-concurrency=complete"])
+            ]
         ),
-    ]
+    ],
+    swiftLanguageVersions: [.version("6"), .v5]
 )
