61245b8684c6ad8f2fd9e68db19104adabc413d0
Magnus Kahr Jensen <magnuskahr@icloud.com>
2025-03-23 21:57:54 +0100

    Fix compile on earlier swift compiler versions (#10)

--- Sources/FlowNavigation/Views/DefaultUnavailableFlowView.swift
+++ Sources/FlowNavigation/Views/DefaultUnavailableFlowView.swift
@@ -5,7 +5,8 @@ public struct DefaultUnavailableFlowView: View {
     public init() {}
     
     public var body: some View {
-        if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *) {
+        #if swift(>=5.9)
+        if #available(iOS 17, macOS 14, tvOS 17, watchOS 10, *) {
             ContentUnavailableView(
                 "No screens available",
                 systemImage: "questionmark.app"
@@ -13,5 +14,8 @@ public struct DefaultUnavailableFlowView: View {
         } else {
             Text("No screens available")
         }
+        #else
+        Text("No screens available")
+        #endif
     }
 }
